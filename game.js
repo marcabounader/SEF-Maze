6c002435ce842ff99878fc7486c30138b7ba3334
marcabounader92 <marcabounader92@outlook.com>
2023-07-14 20:58:01 +0300

    game: added player

--- game.js
+++ game.js
@@ -3,19 +3,20 @@ const gameState = {}
 let maze_container=document.getElementById('maze-subcontainer');
 
 function preload() {
+  // this.load.image('maze', 'src/assets/maze.png');
   this.load.image('player', 'https://content.codecademy.com/courses/learn-phaser/codey.png');
-  this.load.image({
-    key: 'tiles',
-    url: 'src/assets/tilemaps/tiles.png',
-  });
-  this.load.tilemapTiledJSON('maze', 'src/assets/tilemaps/maze.tmj');
+  // this.load.image({
+  //   key: 'tiles',
+  //   url: 'src/assets/tilemaps/tiles.png',
+  // });
+  // this.load.tilemapTiledJSON('maze', 'src/assets/tilemaps/maze.tmj');
 
 }
 
 function create() {
-
-  // gameState.player = this.add.sprite(200, 0.5, 'player');
-  // gameState.player.setOrigin(0,0)
+  gameState.cursors=this.input.keyboard.createCursorKeys();
+  gameState.player = this.add.sprite(200, 0.5, 'player');
+  gameState.player.setOrigin(0,0)
 
 
 }
