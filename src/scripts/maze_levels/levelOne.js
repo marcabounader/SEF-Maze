84fdde35739a93a455fd1a5d70e8c4306c0a31ff
marcabounader92 <marcabounader92@outlook.com>
2023-07-16 17:39:06 +0300

    game: fixed levels

--- src/scripts/maze_levels/levelOne.js
+++ src/scripts/maze_levels/levelOne.js
@@ -5,65 +5,38 @@ export default class levelOne extends Phaser.Scene{
   create(){
     const map=this.make.tilemap({key:"maze-3"});
     const tileset=map.addTilesetImage("TileBasic","wall-tiles")
-    const layer=map.createLayer("layerWall",tileset,200,200)
-    
-    maze_wall.setCollisionByProperty({collides:true});
+    const maze_wall=map.createLayer("layerWall",tileset,200,200)
+
+    maze_wall.setCollisionBetween(0,55);
 
 
     //To Add player+position
     this.player = this.physics.add.sprite(this.game.config.width/2, 0, 'player');
     //To set scale
     this.player.setScale(1,1);
-    
-    //To change Sprite
+    this.cursors=this.input.keyboard.createCursorKeys();
 
-    this.anims.create({
-      key: 'left',
-      frames: this.anims.generateFrameNumbers('player', { start: 66, end: 71 }),
-      frameRate: 10,
-      repeat: -1
-    });
-
-    this.anims.create({
-        key: 'turn',
-        frames: this.anims.generateFrameNumbers('player', { start: 0, end: 5 }),
-        frameRate: 20,
-        repeat: -1
-      });
-
-  this.anims.create({
-      key: 'right',
-      frames: this.anims.generateFrameNumbers('player', { start: 46, end: 51 }),
-      frameRate: 10,
-      repeat: -1
-    });
-
-    this.anims.create({
-      key: 'up',
-      frames: this.anims.generateFrameNumbers('player', { start: 24, end: 29 }),
-      frameRate: 10,
-      repeat: -1
-    });
-
-    this.anims.create({
-      key: 'down',
-      frames: this.anims.generateFrameNumbers('player', { start: 6, end: 11 }),
-      frameRate: 50,
-      repeat: -1
-    });
+ 
 
 
   let debugGraphics=this.add.graphics().setAlpha(0.7);
-      maze_wall.renderDebug(debugGraphics,{
+  maze_wall.renderDebug(debugGraphics,{
           tileColor:null,
           collidingTileColor: new Phaser.Display.Color(243,234,48,255),
           faceColor:new Phaser.Display.Color(40,39,37,255)
       })
 
-    this.cursors=this.input.keyboard.createCursorKeys();
     this.physics.add.collider(this.player,maze_wall,() => {
 
     })
+    this.time.addEvent({
+      delay: 3000,
+      loop: false,
+      callback: () => {
+          this.scene.start("levelTwo");
+      }
+    })
+
 
   }
   update(){
@@ -71,7 +44,6 @@ export default class levelOne extends Phaser.Scene{
       if (this.cursors.left.isDown){
         this.player.setVelocityX(-128);
         this.player.anims.play('left', true);
-        this.scene.start('levelTwo');   
 
       }
       else if (this.cursors.right.isDown){
--- src/scripts/maze_levels/preLoader.js
+++ src/scripts/maze_levels/preLoader.js
@@ -7,7 +7,7 @@ export default class preLoader extends Phaser.Scene{
 
         //Tileheight must be 30!!!
         this.load.image('tiles','src\\assets\\tilemaps\\walls_1x2.png')
-        this.load.tilemapTiledJSON('maze','src\\assets\\tilemaps\\map-1\\maze.json');
+        this.load.tilemapTiledJSON('maze-1','src\\assets\\tilemaps\\map-1\\maze.json');
 
         // Player+Animation (21,26)
         this.load.spritesheet('player', 'src\\assets\\sprites\\player.png', { frameWidth: 21, frameHeight: 26 });
@@ -22,10 +22,49 @@ export default class preLoader extends Phaser.Scene{
         this.load.tilemapTiledJSON("maze-3","src\\assets\\tilemaps\\map-3\\map-3.json") 
 
         this.load.tilemapTiledJSON("maze-4","src\\assets\\tilemaps\\map-4\\map-4.json") 
+        
 
     }
 
     create(){
+           //To change Sprite
+           this.anims.create({
+            key: 'left',
+            frames: this.anims.generateFrameNumbers('player', { start: 66, end: 71 }),
+            frameRate: 10,
+            repeat: -1
+          });
+      
+          this.anims.create({
+              key: 'turn',
+              frames: this.anims.generateFrameNumbers('player', { start: 0, end: 5 }),
+              frameRate: 20,
+              repeat: -1
+            });
+      
+        this.anims.create({
+            key: 'right',
+            frames: this.anims.generateFrameNumbers('player', { start: 46, end: 51 }),
+            frameRate: 10,
+            repeat: -1
+          });
+      
+          this.anims.create({
+            key: 'up',
+            frames: this.anims.generateFrameNumbers('player', { start: 24, end: 29 }),
+            frameRate: 10,
+            repeat: -1
+          });
+      
+          this.anims.create({
+            key: 'down',
+            frames: this.anims.generateFrameNumbers('player', { start: 6, end: 11 }),
+            frameRate: 50,
+            repeat: -1
+          });
+
+      this.cursors=this.input.keyboard.createCursorKeys();
+
         this.scene.start('levelOne');
     }
 
