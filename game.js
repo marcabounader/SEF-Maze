d5b52a0bd2a8debdc5247eb0c6054066a0fba213
marcabounader92 <marcabounader92@outlook.com>
2023-07-16 09:25:15 +0300

    game: added debug and collision

--- game.js
+++ game.js
@@ -15,27 +15,38 @@ export default class Game extends Phaser.Scene{
         const maze_wall=map.createLayer('maze-wall',tileset,100,0);
     
         maze_wall.setCollisionByProperty({collides:true});
+        // maze_wall.setCollisionBetween(0,9);
+
 
         // const mazer=this.add.sprite(128,128,'player','PLAYER_WALK_D')
         this.player = this.add.sprite(200, 0.5, 'player');
         this.player.setScale(0.5,0.5);
         this.cursors=this.input.keyboard.createCursorKeys();
+        this.physics.add.collider(this.player,maze_wall,() => {
+            
+        })
 
+        const debugGraphics=this.add.graphics().setAlpha(0.7);
+        maze_wall.renderDebug(debugGraphics,{
+            tileColor:null,
+            collidingTileColor: new Phaser.Display.Color(243,234,48,255),
+            faceColor:new Phaser.Display.Color(40,39,37,255)
+        })
     }
 
     update(){
         if (this.cursors.left.isDown){
-            this.player.x-=3;
+            this.player.x-=2;
           }
           if (this.cursors.right.isDown){
-            this.player.x+=3;
+            this.player.x+=2;
           }
           if (this.cursors.up.isDown){
-            this.player.y-=3;
+            this.player.y-=2;
         
           }
           if (this.cursors.down.isDown){
-            this.player.y+=3;
+            this.player.y+=2;
           }
     }
 }
