1ecda1635efaa6410b4cb362b35a800b3245a556
marcabounader92 <marcabounader92@outlook.com>
2023-07-16 17:38:57 +0300

    game: fixed levels

--- src/scripts/maze_levels/levelTwo.js
+++ src/scripts/maze_levels/levelTwo.js
@@ -12,45 +12,66 @@ export default class levelTwo extends Phaser.Scene{
         // this.maze.setOrigin(0,0)
         const map=this.make.tilemap({key:'maze-2'});
         const tileset=map.addTilesetImage('walls_1x2','tiles',32,58)
-        const maze_wall=map.createLayer('Tile Layer 1',tileset,100,0);
+        const maze_wall=map.createLayer('Tile Layer 1',tileset,100,50);
     
-        //maze_wall.setCollisionByProperty({collides:true});
-        // maze_wall.setCollisionBetween(0,9);
+        maze_wall.setCollisionBetween(0,8);
 
 
-        // const mazer=this.add.sprite(128,128,'player','PLAYER_WALK_D')
-        this.player = this.add.sprite(200, 0.5, 'player');
-        this.player.setScale(0.5,0.5);
-        this.cursors=this.input.keyboard.createCursorKeys();
-        // this.physics.add.collider(this.player,maze_wall,() => {
-            
-        // })
-        let debugGraphics=this.add.graphics().setAlpha(0.7);
-        maze_wall.renderDebug(debugGraphics,{
-            tileColor:null,
-            collidingTileColor: new Phaser.Display.Color(243,234,48,255),
-            faceColor:new Phaser.Display.Color(40,39,37,255)
-        })
-    }
-
-    update(){
-        if (this.cursors.left.isDown){
-            this.player.x-=2;
-            this.scene.start('levelThree');   
-
-          }
-          if (this.cursors.right.isDown){
-            this.player.x+=2;
-          }
-          if (this.cursors.up.isDown){
-            this.player.y-=2;
+        //To Add player+position
+        this.player = this.physics.add.sprite(this.game.config.width/2, 0, 'player');
+        //To set scale
+        this.player.setScale(1,1);
         
+        this.cursors=this.input.keyboard.createCursorKeys();
+
+    
+    
+      let debugGraphics=this.add.graphics().setAlpha(0.7);
+      maze_wall.renderDebug(debugGraphics,{
+              tileColor:null,
+              collidingTileColor: new Phaser.Display.Color(243,234,48,255),
+              faceColor:new Phaser.Display.Color(40,39,37,255)
+          })
+    
+
+        this.physics.add.collider(this.player,maze_wall,() => {
+    
+        })
+        this.time.addEvent({
+          delay: 3000,
+          loop: false,
+          callback: () => {
+              this.scene.start("levelThree");
           }
-          if (this.cursors.down.isDown){
-            this.player.y+=2;
-          }      
-      
-    }
+        })
+  
+    
+      }
+      update(){
+    
+          if (this.cursors.left.isDown){
+            this.player.setVelocityX(-128);
+            this.player.anims.play('left', true);
+    
+          }
+          else if (this.cursors.right.isDown){
+            this.player.setVelocityX(128);
+            this.player.anims.play('right', true);
+          }
+          else if (this.cursors.up.isDown){
+            this.player.setVelocityY(-128);
+            this.player.anims.play('up', true);
+          }
+          else if (this.cursors.down.isDown){
+            this.player.setVelocityY(128);
+            this.player.anims.play('down', true);
+          }
+          else{
+            this.player.anims.play('turn');
+            this.player.setVelocityY(0);
+            this.player.setVelocityX(0);
+          }
+      }
 
 
 }
