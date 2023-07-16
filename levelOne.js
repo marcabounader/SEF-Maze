cdf8035506b720042b04ef003ff5de2831813548
Rayan Emad <rayanemad508@gmail.com>
2023-07-16 14:14:35 +0300

    Added animation for sprite

--- levelOne.js
+++ levelOne.js
@@ -13,31 +13,49 @@ export default class levelOne extends Phaser.Scene{
 
 
     //To Add player+position
-    this.player = this.add.sprite(200, 300, 'player');
+    this.player = this.physics.add.sprite(300, 300, 'player');
     //To set scale
-    this.player.setScale(2,2);
+    this.player.setScale(1,1);
     
-    //To change image
-    
+    //To change Sprite
+
     this.anims.create({
       key: 'left',
-      frames: this.anims.generateFrameNumbers('player', { start: 0, end: 3 }),
+      frames: this.anims.generateFrameNumbers('player', { start: 66, end: 71 }),
       frameRate: 10,
       repeat: -1
     });
 
-  this.anims.create({
-      key: 'turn',
-      frames: [ { key: 'player', frame: 4 } ],
-      frameRate: 20
-    });
+    this.anims.create({
+        key: 'turn',
+        frames: this.anims.generateFrameNumbers('player', { start: 0, end: 5 }),
+        frameRate: 20,
+        repeat: -1
+      });
 
   this.anims.create({
       key: 'right',
-      frames: this.anims.generateFrameNumbers('player', { start: 5, end: 8 }),
+      frames: this.anims.generateFrameNumbers('player', { start: 46, end: 51 }),
       frameRate: 10,
       repeat: -1
     });
+
+    this.anims.create({
+      key: 'up',
+      frames: this.anims.generateFrameNumbers('player', { start: 24, end: 29 }),
+      frameRate: 10,
+      repeat: -1
+    });
+
+    this.anims.create({
+      key: 'down',
+      frames: this.anims.generateFrameNumbers('player', { start: 6, end: 11 }),
+      frameRate: 10,
+      repeat: -1
+    });
+
+
+
 
     this.cursors=this.input.keyboard.createCursorKeys();
     this.physics.add.collider(this.player,maze_wall,() => {
@@ -48,23 +66,25 @@ export default class levelOne extends Phaser.Scene{
   update(){
 
       if (this.cursors.left.isDown){
-        this.player.x-=2;
+        this.player.setVelocityX(-128);
         this.player.anims.play('left', true);
       }
       else if (this.cursors.right.isDown){
-        this.player.x+=2;
+        this.player.setVelocityX(128);
         this.player.anims.play('right', true);
       }
       else if (this.cursors.up.isDown){
-        this.player.y-=2;
+        this.player.setVelocityY(-128);
         this.player.anims.play('up', true);
       }
       else if (this.cursors.down.isDown){
-        this.player.y+=2;
+        this.player.setVelocityY(128);
         this.player.anims.play('down', true);
       }
       else{
         this.player.anims.play('turn');
+        this.player.setVelocityY(0);
+        this.player.setVelocityX(0);
       }
   }
 }
