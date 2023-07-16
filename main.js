76458dc8ec82cd7a35ee0a8c3313d0fe683a2150
Faissal Moustafa <faissalmoustafa@gmail.com>
2023-07-16 14:20:38 +0300

    level 3

--- main.js
+++ main.js
@@ -6,27 +6,27 @@ import preLoader from './preLoader.js';
 
 let maze_container=document.getElementById('maze-subcontainer');
 
-export default new Phaser.levelOne({
-    type: Phaser.AUTO,
-    width: 1200,
-    height: 850,
-    scene: levelOne
-    })
-// const config = {
-// 	type: Phaser.AUTO,
-// 	width: 1200,
-// 	height: 850,
-// 	backgroundColor: "#5f2a55",
-//     autoCenter: Phaser.Scale.CENTER_BOTH,
-//     parent: maze_container,
-//     physics: {
-//         default: 'arcade',
-//         arcade: {
-//             gravity: { y: 0 },
-//             debug: true
-//         }
-//     },
-//     scene: [preLoader,levelOne,levelTwo]
-// };
+// export default new Phaser.levelOne({
+//     type: Phaser.AUTO,
+//     width: 1200,
+//     height: 850,
+//     scene: levelOne
+//     })
+const config = {
+	type: Phaser.AUTO,
+	width: 1200,
+	height: 850,
+	backgroundColor: "#5f2a55",
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+    parent: maze_container,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 0 },
+            debug: true
+        }
+    },
+    scene: [preLoader,levelOne,levelTwo]
+};
 
-// const game = new Phaser.Game(config);
+const game = new Phaser.Game(config);
