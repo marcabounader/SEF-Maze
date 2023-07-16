0fea567155e0a1fc93c5056d0a470662a76ee7b0
Rayan Emad <rayanemad508@gmail.com>
2023-07-16 13:00:43 +0300

    Add the atlas file to help with animation

--- preLoader.js
+++ preLoader.js
@@ -7,7 +7,9 @@ export default class preLoader extends Phaser.Scene{
 
         this.load.image('tiles','src\\assets\\tilemaps\\walls_1x2.png')
         this.load.tilemapTiledJSON('maze','src\\assets\\tilemaps\\map-1\\maze.json');
-        this.load.spritesheet('player', 'src\\assets\\sprites\\player.png', { frameWidth: 20, frameHeight: 32 });
+        // Player+Animation
+        this.load.spritesheet('player', 'src\\assets\\sprites\\player.png', { frameWidth: 18, frameHeight: 25 });
+        this.load.atlas('player', 'src\\assets\\sprites\\player.png', 'src\\assets\\sprites\\player.json')
     }
 
     create(){
