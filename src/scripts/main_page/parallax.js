4d507f0e42bf7252c6d3f3db636f59499670c818
Rayan Emad <rayanemad508@gmail.com>
2023-07-17 09:46:21 +0300

    Chnaged sprite image for parallax

--- src/scripts/main_page/parallax.js
+++ src/scripts/main_page/parallax.js
@@ -1,15 +1,14 @@
 let stars = document.getElementById('stars');
-let moon = document.getElementById('moon');
+let sprite = document.getElementById('sprite');
 let mountains_behind = document.getElementById('mountains_behind');
 let mountains_front = document.getElementById('mountains_front');
 let text = document.getElementById('text');
-let btn = document.getElementById('btn');
 let header = document.querySelector('header');
 
 window.addEventListener('scroll', function(){
     let value = window.scrollY;
     stars.style.left = value * 0.25 + 'px';
-    moon.style.top = value * 1.05 + 'px';
+    sprite.style.top = value * 1.05 + 'px';
     mountains_behind.style.top = value * 0.5 + 'px';
     mountains_front.style.top = value * 0 + 'px';
     text.style.marginRight = value * 6 + 'px';
